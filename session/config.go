ed196be79c917cb4cf67f8d4ec02de0e3c802ad5
elmyrockers <elmyrockers@gmail.com>
2026-07-30 23:10:21 +0800

    Set attribute of Config struct: `SessionConfig`

--- session/config.go
+++ session/config.go
@@ -2,9 +2,8 @@ package session
 
 import (
 	"github.com/gofiber/fiber/v3/middleware/session"
-	"github.com/gofiber/storage"
 )
 
 type Config struct {
-	SessionStorage storage.Storage
+	SessionConfig session.Config
 }
