6f23e2ac0aa6e5465a57da23a2ac9eabe87092ae
elmyrockers <elmyrockers@gmail.com>
2026-07-31 22:00:45 +0800

    Update `Config` struct: include `MailConfig` attribute

--- session/config.go
+++ session/config.go
@@ -4,8 +4,36 @@ import (
 	"github.com/gofiber/fiber/v3/middleware/session"
 )
 
+type MailAddress struct {
+	Name  string
+	Email string
+}
+
+const (
+	EmailVerification = "email_verification"
+	PasswordReset = "password_reset"
+)
+
+type MailTemplate struct {
+    Subject string
+    Body    string
+    AltBody string
+}
+
+
+
+type MailConfig struct {
+	From MailAddress
+	ReplyTo *MailAddress
+	Templates map[string]MailTemplate
+}
+
+
+
+
 type Config struct {
 	SessionConfig session.Config
-	DatabaseAdapter Database
-	MailAdapter Mail
+	DatabaseAdapter DatabaseAdapter
+	MailAdapter MailAdapter
+	MailConfig MailConfig
 }
