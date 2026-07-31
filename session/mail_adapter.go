8de8a40a5c634fa54449f0db7634f0631872ed22
elmyrockers <elmyrockers@gmail.com>
2026-07-31 22:47:58 +0800

    Add 5 methods to `MailAdapter` interface: `SetFrom(), SetReplyTo(), SetTo(), SetTemplate()` and `Send()`

--- session/mail_adapter.go
+++ session/mail_adapter.go
@@ -4,5 +4,11 @@ package session
 
 
 type MailAdapter interface {
-	
+    SetFrom(address MailAddress)
+    SetTo(address MailAddress)
+    SetReplyTo(address MailAddress)
+
+    SetTemplate(template MailTemplate)
+
+    Send() error
 }
