61b2a152e9b5fbf8936fde72b2d830485b572851
elmyrockers <elmyrockers@gmail.com>
2026-08-01 18:02:00 +0800

    Add 1 private method and 1 middleware: `unauthenticated()` and `AuthRequired()`

--- session/auth.go
+++ session/auth.go
@@ -1,7 +1,7 @@
 package session
 
 import (
-	// "github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3"
 	fibersession "github.com/gofiber/fiber/v3/middleware/session"
 
 	// "github.com/davecgh/go-spew/spew"
@@ -12,6 +12,7 @@ type Auth struct {
 	sessionStore *fibersession.Store
 	dbAdapter DatabaseAdapter
 	mailAdapter MailAdapter
+	loginUrl string
 }
 
 func New(config ...Config) *Auth {
@@ -20,10 +21,40 @@ func New(config ...Config) *Auth {
 		if len(config) > 1 { cfg = config[0] }
 
 	// Set up Auth attributes, then return it
+		loginURL := cfg.LoginURL
+		if loginURL == nil { loginURL = "/auth/login" }
+
 		session := fibersession.NewStore( cfg.SessionConfig )
 		return &Auth{
 			sessionStore: session,
 			dbAdapter: cfg.DatabaseAdapter,
 			mailAdapter: cfg.MailAdapter,
+			loginURL: loginURL,
 		}
 }
+
+// Redirects user to login url if unauthenticated
+func (a *Auth) unauthenticated(c fiber.Ctx) error {
+	if c.Accepts("json") == "json" {
+		return fiber.ErrUnauthorized
+	}
+
+	return c.Redirect(a.LoginURL)
+}
+
+// MIDDLEWARE ----------------------------------------------------------------------
+func (a *Auth) AuthRequired(c fiber.Ctx) error {
+	// Make sure user_id exists
+		sess, err := a.sessionStore.Get(c)
+		if err != nil { return err }
+
+		userID := sess.Get("user_id")
+		if userID == nil {
+			return a.unauthenticated(c)
+		}
+
+	return c.Next()
+}
+
+
+// ---------------------------------------------------------------------------------
