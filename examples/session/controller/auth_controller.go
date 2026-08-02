c4848490d035b71a6625eee6c0b537596f292f47
elmyrockers <elmyrockers@gmail.com>
2026-08-02 08:19:59 +0800

    Add AuthController for examples

---
+++ examples/session/controller/auth_controller.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"github.com/gofiber/fiber/v3"
+)
+
+type AuthController struct {}
+
+func NewAuth() *AuthController {
+	return &AuthController{}
+}
+
+func (ac *AuthController) Register(c fiber.Ctx) error {
+	return c.Render("register", fiber.Map{
+				"Title": "Register Form",
+			})
+}
+
+func (ac *AuthController) Login(c fiber.Ctx) error {
+	return nil
+}
