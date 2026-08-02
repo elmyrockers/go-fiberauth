5a1e13478b6ce05a0f4d7f60b95c26d4c6ebbd75
elmyrockers <elmyrockers@gmail.com>
2026-08-03 01:15:23 +0800

    Rename a few methods of AuthController

--- examples/session/controller/auth_controller.go
+++ examples/session/controller/auth_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/csrf"
 )
 
 type AuthController struct {}
@@ -10,20 +11,31 @@ func NewAuth() *AuthController {
 	return &AuthController{}
 }
 
-func (ac *AuthController) Register(c fiber.Ctx) error {
+func (ac *AuthController) RegisterPage(c fiber.Ctx) error {
 	return c.Render("register", fiber.Map{
 				"Title": "Register Form",
+				"csrf": csrf.TokenFromContext(c),
 			})
 }
 
-func (ac *AuthController) Login(c fiber.Ctx) error {
+func (ac *AuthController) LoginPage(c fiber.Ctx) error {
 	return c.Render("login", fiber.Map{
 				"Title": "Login Form",
+				"csrf": csrf.TokenFromContext(c),
 			})
 }
 
-func (ac *AuthController) ForgotPassword(c fiber.Ctx) error {
+func (ac *AuthController) ForgotPasswordPage(c fiber.Ctx) error {
 	return c.Render("forgot-password", fiber.Map{
 				"Title": "Forgot Password Form",
+				"csrf": csrf.TokenFromContext(c),
+			})
+}
+
+func (ac *AuthController) ResetPasswordPage(c fiber.Ctx) error {
+	return c.Render("reset-password", fiber.Map{
+				"Title": "Reset Password Form",
+				"csrf": csrf.TokenFromContext(c),
+				"token": "",
 			})
 }
