4de80e6c9a433e31eae0ede3bcb0c0a00cf7a456
elmyrockers <elmyrockers@gmail.com>
2026-08-05 03:02:34 +0800

    Update routes

--- examples/session/main.go
+++ examples/session/main.go
@@ -28,21 +28,23 @@ func main(){
 		Extractor: extractors.FromForm("_csrf"),
 	}))
 
-
-	app.Get("/auth/login", authController.LoginPage )
-	app.Get("/auth/register", authController.RegisterPage )
-	app.Get("/auth/forgot-password", authController.ForgotPasswordPage )
-	app.Get("/auth/reset-password", authController.ResetPasswordPage )
-
-
-
-	app.Post("/auth/login", authController.Login)
-	app.Post("/auth/register", authController.Register)
-	app.Post("/auth/forgot-password", authController.ForgotPassword )
+	// Routes
+		app.Get("/auth/login", authController.LoginPage )
+		app.Get("/auth/register", authController.RegisterPage )
+		app.Get("/auth/forgot-password", authController.ForgotPasswordPage )
+		app.Get("/auth/reset-password", authController.ResetPasswordPage )
+		app.Get("/auth/password/confirm", authController.PasswordConfirmationPage )
+		app.Get("/auth/2fa/challenge", authController.TwoFactorAuthChallengePage )
 
 
 
-	app.Get("/member/index", auth.AuthRequired, Hello)
+		app.Post("/auth/login", authController.Login)
+		app.Post("/auth/register", authController.Register)
+		app.Post("/auth/forgot-password", authController.ForgotPassword )
+
+
+
+		app.Get("/member/index", auth.AuthRequired, Hello)
 	// app.Post("/auth/register", auth.GuestOnly, auth.RateLimiter(5, time.Minute), authController.register )
 
 
