f69706a308d04be593da4fe50086273e87417d8b
elmyrockers <elmyrockers@gmail.com>
2026-08-10 11:23:58 +0800

    Update routes

--- examples/session/main.go
+++ examples/session/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/csrf"
 	"github.com/gofiber/fiber/v3/extractors"
 	"github.com/gofiber/template/jet/v3"
 	fiberauth "github.com/elmyrockers/go-fiberauth/session"
+	"github.com/elmyrockers/go-fiberauth/adapters/db/mysql"
 
 	"github.com/elmyrockers/go-fiberauth/examples/session/controller"
 
@@ -17,10 +19,20 @@ func Hello(c fiber.Ctx) error {
 }
 
 func main(){
-	auth := fiberauth.New()
-	_ = auth
+	db, err := mysql.New(mysql.Config{
+								Host:     "127.0.0.1",
+								User:     "root",
+								Password: "",
+								DBName:   "fiberauth",
+								Port:   3307,
+							})
+	if err != nil { log.Fatal(err) }
 
-	authController := controller.NewAuth()
+	auth := fiberauth.New(fiberauth.Config{
+		DatabaseAdapter: db,
+	})
+
+	authController := controller.NewAuth( auth )
 
 	engine := jet.New( "views", ".jet" )
 	app := fiber.New(fiber.Config{ Views: engine })
@@ -29,20 +41,21 @@ func main(){
 	}))
 
 	// Routes
-		app.Get("/auth/login", authController.LoginPage )
-		app.Get("/auth/register", authController.RegisterPage )
-		app.Get("/auth/forgot-password", authController.ForgotPasswordPage )
-		app.Get("/auth/reset-password", authController.ResetPasswordPage )
-		app.Get("/auth/password/confirm", authController.PasswordConfirmationPage )
-		app.Get("/auth/2fa/recovery-codes", authController.TwoFactorAuthRecoveryCodesPage )
-		app.Get("/auth/2fa/challenge", authController.TwoFactorAuthChallengePage )
+		// Display Auth forms
+			app.Get("/auth/register", authController.RegisterPage )
+			app.Get("/auth/login", authController.LoginPage )
+			
+			app.Get("/auth/forgot-password", authController.ForgotPasswordPage )
+			app.Get("/auth/reset-password", authController.ResetPasswordPage )
+			app.Get("/auth/password/confirm", authController.PasswordConfirmationPage )
+			app.Get("/auth/2fa/recovery-codes", authController.TwoFactorAuthRecoveryCodesPage )
+			app.Get("/auth/2fa/challenge", authController.TwoFactorAuthChallengePage )
 		
 
-
-
-		app.Post("/auth/login", authController.Login)
-		app.Post("/auth/register", authController.Register)
-		app.Post("/auth/forgot-password", authController.ForgotPassword )
+		// Process Auth forms
+			app.Post("/auth/register", authController.Register)
+			app.Post("/auth/login", authController.Login)
+			app.Post("/auth/forgot-password", authController.ForgotPassword)
 
 
 
