495b07c2257b0389c522c3a9cc82ab5f18d0ee3d
elmyrockers <elmyrockers@gmail.com>
2026-08-12 19:36:14 +0800

    Update validation rule

--- examples/session/form/register.go
+++ examples/session/form/register.go
@@ -5,5 +5,5 @@ type RegisterForm struct {
 	Name                  string `json:"name" form:"name" validate:"required,max=255"`
 	Email                 string `json:"email" form:"email" validate:"required,email,max=255"`
 	Password              string `json:"password" form:"password" validate:"required,min=8"`
-	PasswordConfirmation  string `json:"password_confirmation" form:"password_confirmation" validate:"required,eqfield=password"`
+	PasswordConfirmation  string `json:"password_confirmation" form:"password_confirmation" validate:"required,eqfield=Password"`
 }
