63fc293d52a4a6187b20ef07d568da62eef6d959
elmyrockers <elmyrockers@gmail.com>
2026-09-01 02:22:42 +0800

    Add `Auth` struct

--- xvelope.go
+++ xvelope.go
@@ -1,3 +1,15 @@
 package xvelope
 
+type Config struct{}
 
+type Auth struct {
+	context HttpContext
+}
+
+func New( config ...Config) *Auth {
+	return &Auth{}
+}
+
+func (a *Auth) SetHttpContext( ctx HttpContext) {
+	a.context = ctx
+}
